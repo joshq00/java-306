23b9833e1204c30772978e63c526faf6c2200e8b
Josh Quintana <josh_d_quintana@homedepot.com>
2015-03-05 10:04:03 -0500

    Show/hide delete button and delete when it's clicked

--- src/item-list.jsx
+++ src/item-list.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import itemStore from './item-store';
 import action from './item-action';
+import actions from './item-action-types';
 
 /**
  * Check if a touch is within
- * the bounds of a rect
+ * the bounds of a dom node
  *
- * @param  {ClientRects} coords - the boundaries
  * @param  {Touch} touch
+ * @param  {HTMLElement} domNode - the container node
  * @return {boolean} is the touch within the bounds?
  */
-function within ( coords, touch ) {
+function within ( touch, domNode ) {
+	let coords = domNode.getClientRects()[ 0 ];
 	let { top, bottom, left, right } = coords;
 	let { clientX: x, clientY: y } = touch;
 
@@ -33,18 +35,44 @@ let ListItem = React.createClass({
 			// how far must they move
 			minOffset: 50,
 			// how long do they have (ms)
-			maxDelay: 300
+			maxDelay: 500
 		};
+	},
+	getInitialState () {
+		return {
+			touch: undefined,
+			left: 0
+		};
+	},
+
+	componentDidMount () {
+		document.addEventListener( 'touchstart', this._onDocumentTouch );
+	},
+	componentWillUnmount () {
+		document.removeEventListener( 'touchstart', this._onDocumentTouch );
+	},
+
+	/**
+	 * Listen to document touch so that
+	 * if a touch happens outside of this node
+	 * we can cancel any touch state
+	 */
+	_onDocumentTouch ( e ) {
+		let target = e.target;
+		let domNode = this.getDOMNode();
+		while ( target != null && target !== domNode ) {
+			target = target.parentNode;
+		}
+		if ( domNode !== target ) {
+			this._clearTouch();
+		}
 	},
 
 	/**
 	 * Clear the stored touch
 	 */
 	_clearTouch () {
-		if ( arguments.length ) console.log( 'clearing' );
-		this.setState({
-			touch: undefined
-		});
+		this.setState( this.getInitialState() );
 	},
 
 	/**
@@ -67,7 +95,7 @@ let ListItem = React.createClass({
 	 *
 	 * @param  {TouchEvent}
 	 */
-	_touchStart ({ touches, targetTouches }) {
+	_onTouchStart ({ touches, targetTouches }) {
 		clearTimeout( this.state ? this.state.clear : null );
 		if ( touches.length > 1 ) {
 			// swipe only happens
@@ -85,14 +113,26 @@ let ListItem = React.createClass({
 		});
 	},
 
+	_onTouchMove ({ touches, changedTouches }) {
+		let touch = changedTouches[ 0 ];
+		// are there multiple touches?
+		let multiple = ( touches.length > 1 );
+		// is the first touch in bounds?
+		let inBounds = within( touch, this.getDOMNode() );
+		if ( multiple || !inBounds || !this.state.touch ) {
+			this._clearTouch();
+			return;
+		}
+		// this.setState({ left: touch.clientX - this.state.touch.clientX });
+	},
+
 	/**
 	 * Handle a touchEnd
 	 *
 	 * @param  {TouchEvent}
 	 */
-	_touchEnd ({ touches, changedTouches }) {
+	_onTouchEnd ({ touches, changedTouches }) {
 		clearTimeout( this.state ? this.state.clear : null );
-		let rects = this.getDOMNode().getClientRects()[ 0 ];
 		let { touch } = this.state;
 		let changedTouch = changedTouches[ 0 ];
 
@@ -104,12 +144,11 @@ let ListItem = React.createClass({
 		// it's a different touch
 		case changedTouch.identifier !== touch.identifier:
 		// it's not in our coords
-		case !within( rects, changedTouch ):
-			// clear the stored touch
-			this._clearTouch();
-			// and do nothing
+		case !within( changedTouch, this.getDOMNode() ):
+			// do nothing
 			return;
 		}
+		this._clearTouch();
 
 		let { clientX: startX, clientY: startY } = touch;
 		let { clientX: endX,   clientY: endY   } = changedTouch;
@@ -126,16 +165,33 @@ let ListItem = React.createClass({
 
 		if ( xdiff < 0 ) {
 			// swiped left
-			// show delete!
-			console.log( 'delete' );
+			// show modifiers!
+			this.setState({
+				// TODO: figure out how to do this without
+				// fixed offset
+				left: -50
+			});
 		}
 	},
 
-	render() {
+	_delete () {
+		action.fire( actions.REMOVE, this.props.item );
+	},
+
+	render () {
+		let divStyle = {
+			left: ~~this.state.left
+		};
+		let deleteButton = <button
+			className='delete'
+			onClick={this._delete}>Delete</button>
+
 		return (
 			<div
-				onTouchStart={this._touchStart}
-				onTouchEnd={this._touchEnd}
+				style={divStyle}
+				onTouchStart={this._onTouchStart}
+				onTouchMove={this._onTouchMove}
+				onTouchEnd={this._onTouchEnd}
 				className='listItem'>
 
 				<span className='title'>
@@ -146,6 +202,7 @@ let ListItem = React.createClass({
 					{this.props.item.detail}
 				</span>
 
+				{deleteButton}
 			</div>
 		);
 	}
@@ -159,6 +216,12 @@ function getItemState () {
 export default React.createClass({
 	getInitialState () {
 		return getItemState();
+	},
+	componentDidMount () {
+		itemStore.on( this._onChange );
+	},
+	componentWillUnmount () {
+		itemStore.off( this._onChange );
 	},
 	_onChange () {
 		this.setState( getItemState() );
--- src/item-store.js
+++ src/item-store.js
@@ -1,5 +1,5 @@
 import dispatcher from './dispatcher';
-import actionTypes from './item-action-types';
+import actions from './item-action-types';
 import { EventEmitter } from 'events';
 import _items from './items.json';
 
@@ -9,16 +9,33 @@ let _activeItem = {
 	detail: undefined
 };
 
+function remove ( item ) {
+	delete _items[ item.id ];
+}
 class ItemStore extends EventEmitter {
 	constructor () {
 		super();
 		this._token = dispatcher.register( action => {
+			let { type, payload } = action;
+			switch ( type ) {
+				case actions.REMOVE:
+					remove( payload );
+					this.emitChange();
+					break;
+			}
 			console.log( action );
 		});
 	}
 
-	off ( event, listener ) {
-		this.removeListener( event, listener );
+	emitChange () {
+		this.emit( actions.CHANGE );
+	}
+
+	on ( listener ) {
+		return super.on( actions.CHANGE, listener );
+	}
+	off ( listener ) {
+		this.removeListener( actions.CHANGE, listener );
 	}
 
 	getAll () {
